b59ed7d67cd4b901e5e77327778aaa152500fae3
sidharth-n <sidharthnofficial@gmail.com>
2023-06-08 13:58:11 +0530

    naming fixed with itm and pid

--- 3D/3dExtractor.js
+++ 3D/3dExtractor.js
@@ -2,26 +2,39 @@ const puppeteer = require("puppeteer");
 const request = require("request");
 const fs = require("fs");
 const urlModule = require("url");
+const path = require("path");
 const requestPromise = require("request-promise");
 
 // Initialize results array
 let results = [];
 
 async function extract3DModel(url) {
+  const parsedUrl = new urlModule.URL(url);
+
+  // Extract PID and ITM from URL
+  let urlParts = url.split("/");
+  let itm = urlParts[urlParts.length - 1].split("?")[0];
+  let pid = parsedUrl.searchParams.get("pid");
+
+  // Create the Flipkart directory if it doesn't exist
+  const directoryPath = path.join(__dirname, "flipkart");
+  fs.mkdirSync(directoryPath, { recursive: true });
+
+  const fileName = `${itm}-${pid}.glb`;
+  const filePath = path.join(directoryPath, fileName);
+
+  // Check if the model is already downloaded
+  const existingResults = JSON.parse(fs.readFileSync("results.json", "utf8"));
+  if (existingResults.some((result) => result.fileName === fileName)) {
+    console.log(`Model ${fileName} already exists.`);
+    return;
+  }
+
   // Launch the puppeteer browser
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2" });
 
-  const parsedUrl = new urlModule.URL(url);
-  // Extract PID and ITM from URL
-  let urlParts = url.split("/");
-  //let itm = urlParts[urlParts.length - 2].split("?")[0];
-  let itm = urlParts[urlParts.length - 1].split("?")[0];
-  let pid = parsedUrl.searchParams.get("pid");
-  console.log("urlParts :" + urlParts);
-  console.log("itm : " + itm);
-  console.log("pid : " + pid);
   // Search for 3D model URL
   const modelUrls = await page.evaluate(() => {
     let urls = [];
@@ -44,19 +57,21 @@ async function extract3DModel(url) {
 
   // Download 3D model if available
   if (modelUrls.length > 0) {
-    const fileName = `${itm}-${pid}.glb`;
     request(modelUrls[0])
-      .pipe(fs.createWriteStream(fileName))
+      .pipe(fs.createWriteStream(filePath))
       .on("close", () => {
-        console.log(`${fileName} downloaded successfully.`);
+        console.log(`${filePath} downloaded successfully.`);
         results.push({
           url: url,
           hasModel: true,
           downloaded: true,
-          fileName: fileName,
+          fileName: filePath,
         });
-        // Write the results to a JSON file
-        fs.writeFileSync("results.json", JSON.stringify(results, null, 4));
+        // Append the results to the JSON file
+        fs.writeFileSync(
+          "results.json",
+          JSON.stringify([...existingResults, ...results], null, 4)
+        );
       });
   } else {
     results.push({
@@ -65,11 +80,16 @@ async function extract3DModel(url) {
       downloaded: false,
       fileName: null,
     });
-    // Write the results to a JSON file
-    fs.writeFileSync("results.json", JSON.stringify(results, null, 4));
+    // Append the results to the JSON file
+    fs.writeFileSync(
+      "results.json",
+      JSON.stringify([...existingResults, ...results], null, 4)
+    );
   }
 }
 
-extract3DModel(
-  "https://www.flipkart.com/nothing-phone-1-black-256-gb/p/itmeea53a564de47?pid=MOBGCYGPWXYRRNB4&lid=LSTMOBGCYGPWXYRRNB426WTZ8&marketplace=FLIPKART&store=tyy%2F4io&pageUID=1686205138161"
-);
+// Load URLs from JSON file
+const urls = JSON.parse(fs.readFileSync("urls.json", "utf8"));
+
+// Iterate through each URL and extract the 3D model
+urls.forEach((url) => extract3DModel(url));
